64f0211225371bfd7a6925c739a72ad35cc81c22
uvsq21707861 <moulhat.ahmed@ens.uvsq.fr>
2021-09-30 14:36:36 +0200

    Methode toString

--- Fraction.java
+++ Fraction.java
@@ -1,2 +1,15 @@
 public class Fraction{
+	
+	private int numerateur;
+	private int denominateur;
+	
+	Fraction(int numer, int denom){
+		numerateur = numer;
+		denominateur = denom;
+	}
+	
+	public String toString() {
+		System.out.println(numerateur + "/" + denominateur);
+		return numerateur + "/" + denominateur;
+	}
 }
--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ public class Main{
 	public static void main(String[] args){
 		
 		System.out.println("Test");
+		
+		Fraction F = new Fraction(50,8);
+		F.toString();
 	}
 }
